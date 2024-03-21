69cf8a0feb19ede360560fca685bdb4db9606f4b
dantealegria1 <dante.romero18@gmail.com>
2024-03-20 19:49:21 -0600

    20-03

--- Codigos/Aprendizaje_inteligente/Unidad_2/Limpiar_Palabras.py
+++ Codigos/Aprendizaje_inteligente/Unidad_2/Limpiar_Palabras.py
@@ -1,23 +1,32 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-
 import pandas as pd
 
 nltk.download('punkt')
 nltk.download('stopwords')
+global PROHIBIDAS
+PROHIBIDAS = ["br"]
 
-def leer (archivo):
-    texto = pd.read_csv(archivo, sep='"')
+def leer(archivo):
+    texto = pd.read_csv(archivo, sep=',')
     print(texto.head())
 
 def texto_a_vector(texto):
     palabras = word_tokenize(texto)
     stopwords_ingles = set(stopwords.words('english'))
     palabras_filtradas = [palabra.lower() for palabra in palabras 
-                        if palabra.isalnum() and palabra.lower() not in 
-                        stopwords_ingles]
+                          if palabra.isalpha() and palabra.lower() 
+                          not in stopwords_ingles and palabra.lower() not in PROHIBIDAS]
     return palabras_filtradas
 
 
-leer("Peliculas.csv")
+def procesar_texto(archivo):
+    texto = pd.read_csv(archivo)
+    #Solo leer la primer fila
+    texto = texto.iloc[0,0]
+    palabras = texto_a_vector(texto)
+    print(palabras)
+
+
+procesar_texto("Peliculas.csv")
